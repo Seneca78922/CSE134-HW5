56afa82eb2d3d6dcd88a92aa0d410dad35f0c8d7
Seneca78922 <vsasaoka@gmail.com>
2023-03-08 16:28:07 -0800

    XMLHttpRequest stub

--- methodtest.js
+++ methodtest.js
@@ -8,27 +8,44 @@ export function initBtnListeners() {
 
 }
 /*each button will set the method and action of the form, submit the form, grab the JSON response data and convert it to html*/
+  //data from endpoint is JSON response to be parsed into string, put into output tag with id value of response
 function JSONToHTMLTable(response) {
 
 }
-function sendHTTPRequest() { //maybe one for XMLHTTPRequest and one with fetch API
-
+function sendHTTPRequest(method,urlString) { //maybe one for XMLHTTPRequest and one with fetch API
+    let xhr = new XMLHttpRequest();
+    xhr.open(method,urlString,true);
+    xhr.responseType = 'json';
+    //let formData = new FormData(document.getElementById("form"));
+    xhr.send(); //need to get form data and send it
+   // console.log(...formData);
+    //remove this tester code
+    xhr.onload = function() {
+        if (xhr.status != 200) { // HTTP error?
+          // handle error
+          alert( 'Error: ' + xhr.status);
+          return;
+        }
+        else {
+            alert(xhr.response);
+        }
+      
+        // get the response from xhr.response
+      };
 }
 function postBtnEvent() {
-    document.querySelector('form').action = 'https://httpbin.org/post';
-    document.querySelector('form').method = 'POST'; 
-    //data from endpoint is JSON response to be parsed into string, put into output tag with id value of response
+   //only send form data in post request
+  
 
 }
 function getBtnEvent() {
-    document.querySelector('form').action = 'https://httpbin.org/get';
-    document.querySelector('form').method = 'GET'; 
+    
+    sendHTTPRequest('GET','https://httpbin.org/get');
+    JSONToHTMLTable();
 }
 function putBtnEvent() {
-    document.querySelector('form').action = 'https://httpbin.org/put';
-    document.querySelector('form').method = 'PUT'; 
+    
 }
 function deleteBtnEvent() {
-    document.querySelector('form').action = 'https://httpbin.org/delete';
-    document.querySelector('form').method = 'DELETE'; 
+    
 }
