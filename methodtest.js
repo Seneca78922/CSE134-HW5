78e9e6c16caa6c65334c231cd3f52e7605a3df10
Seneca78922 <vsasaoka@gmail.com>
2023-03-13 18:31:42 -0700

    added to nav

--- methodtest.js
+++ methodtest.js
@@ -13,8 +13,7 @@ export function updateDate() {
   let date = document.getElementById("date_time");
   date.value = new Date();
 }
-/*each button will set the method and action of the form, submit the form, grab the JSON response data and convert it to html*/
-  //data from endpoint is JSON response to be parsed into string, put into output tag with id value of response
+ 
 function createSubTable(output,currValue){
   let subTable = document.createElement("table");
   subTable.appendChild(document.createTextNode("{"));
@@ -30,23 +29,26 @@ function createSubTable(output,currValue){
     subTable.appendChild(document.createTextNode("},\n"));
     currValue.appendChild(subTable);
 }
-
+/*
+Loop through each row of key,value pairs in JSON response and convert that into rows in an HTML table. 
+Have nested tables for key value pairs for headers, args, form, etc. Append table to <output> area. 
+*/
 function JSONToHTMLTable(response,method) {
-
+   
     let outputArea = document.getElementById("response");
     outputArea.innerHTML = "<p>HTTP Response<p>"; //center this
 
     let JSONOutput = JSON.parse(response);
     let table = document.createElement("table");
     table.appendChild(document.createTextNode("{"));
-    //table.appendChild(document.createTextNode("{"));
+    
     Object.entries(JSONOutput).forEach(([key,value]) => {
       let currRow = table.insertRow();
       let currKey = currRow.insertCell();
       let currValue = currRow.insertCell();
       currKey.innerHTML = JSON.stringify(key);
       console.log(currKey.innerHTML);
-      //currValue.innerHTML = JSON.stringify(value);
+      
       if (key == "form") {
         createSubTable(JSONOutput.form,currValue);
       }
@@ -63,66 +65,63 @@ function JSONToHTMLTable(response,method) {
         currValue.innerHTML = JSON.stringify(value);
         if (key != "url") {
            currValue.append(",");
-        }
-        
+        }   
       }
 
   });
   table.appendChild(document.createTextNode("}"));
-
   outputArea.appendChild(table);
   
-
-
 }
-   
-function sendHTTPRequest(method,urlString) { //maybe one for XMLHTTPRequest and one with fetch API
+
+function sendXMLHTTPRequest(method,urlString) {
+  
     let xhr = new XMLHttpRequest();
     xhr.open(method,urlString);
-    //xhr.responseType = 'json';
-    //let formData = new FormData(document.getElementById("form"));
+    
     if (method == 'GET'){
-      xhr.send(); //need to get form data and send it
+      xhr.send(); 
     }
-    else if (method == 'POST'){
+    //if post or put request, set some http request headers and send the form data to endpoint
+    else if (method == 'POST' || method == 'PUT'){
       console.log(document.forms.form);
-      let formData = new FormData(document.forms.form);
-      xhr.send(formData);
-    }
-    else if (method == "PUT") {
-      let formData = new FormData(document.forms.form);
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      xhr.setRequestHeader("X-Sent-By", "javascript");
+      
+      let id = document.forms.form.id.value;
+      let article_name = document.forms.form.article_name.value;
+      let article_body = document.forms.form.article_body.value;
+      let date_time = document.forms.form.date_time.value;
+      let formData =  `id=${id}&article_name=${article_name}&article_body=${article_body}&date_time=${date_time}`;
       xhr.send(formData);
     }
     else {
-      //DELETE
+      //DELETE request, not sending any form data
       xhr.send(null);
-
     }
     xhr.onreadystatechange = function() {
       if (xhr.readyState === 4 && xhr.status === 200) {
-        console.log(xhr.responseText); //remove 
+        //console.log(xhr.responseText); 
+        //If receive http response, then convert response from JSON into html table
         JSONToHTMLTable(xhr.responseText,method);
       }
   }
-  
-    
+      
 }
 function postBtnEvent() {
    //only send form data in post request
-   sendHTTPRequest('POST','https://httpbin.org/post');
-
-  
+   sendXMLHTTPRequest('POST','https://httpbin.org/post');
 
 }
 function getBtnEvent() {
-  //normally woudl do /get/idOfForm
-     sendHTTPRequest('GET','https://httpbin.org/get');
+  //normally would do /get/idOfForm
+     sendXMLHTTPRequest('GET','https://httpbin.org/get');
 }
 function putBtnEvent() {
   //normally would do /put/idOfForm
-  sendHTTPRequest('PUT','https://httpbin.org/put');
+  sendXMLHTTPRequest('PUT','https://httpbin.org/put');
 }
 function deleteBtnEvent() {
   //normally would do /delete/idOfForm
-  sendHTTPRequest('DELETE','https://httpbin.org/delete');
+  sendXMLHTTPRequest('DELETE','https://httpbin.org/delete');
 }
